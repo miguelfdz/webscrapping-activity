0018ad12d8c1d6486b3356662b6b1d700b0de217
miguelfdz <miguelfdzm08@gmail.com>
2021-03-17 18:06:25 -0600

    WIP

--- app/controllers/search_product_controller.rb
+++ app/controllers/search_product_controller.rb
@@ -4,15 +4,23 @@ class SearchProductController < ApplicationController
   end
 
   def create
-    link = SearchProducts::LinkResult.generate_link(params[:search_product])
-    add_link_to_txt_file(link)
-    sleep(5)
-    SearchProducts::LinkResult.scrap
-    sadjhkjh
-    redirect_to show_search_product_path
+    @search_product = SearchProduct.new(get_params(params[:search_product]))
+    if @search_product.save
+      add_link_to_txt_file(link)
+      SearchProducts::LinkResult.scrap
+      redirect_to show_search_product_path
+    end
   end
 
   private
+  def get_params(params)
+    {
+      "product" => params[:product],
+      "sort" => params[:sort],
+      "quantity" => params[:quantity],
+      "link" => SearchProducts::LinkResult.generate_link(params)
+    }
+  end
 
   def add_link_to_txt_file(link)
     file = File.open('pcel_link.txt', 'w') { |f|
